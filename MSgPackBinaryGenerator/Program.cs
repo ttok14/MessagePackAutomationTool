833f78438ad3e11c5b2cbf6bfcaff174adb4911b
ttok14 <ttok14@naver.com>
2025-10-17 13:31:39 +0900

    결과물 확장자 변경 (.bin)

--- MSgPackBinaryGenerator/Program.cs
+++ MSgPackBinaryGenerator/Program.cs
@@ -258,7 +258,7 @@ namespace MSgPackBinaryGenerator
                     }
 
                     // 5. 생성된 옵션을 메서드에 파라미터로 전달합니다.
-                    string binaryOutputPath = Path.Combine(binaryOutputDirectory, $"{container.SchemaData.TableName}.bytes");
+                    string binaryOutputPath = Path.Combine(binaryOutputDirectory, $"{container.SchemaData.TableName}.bin");
 
                     Console.WriteLine("\n--- Starting Serialization ---\n");
                     method.Invoke(null, new object[] { binaryOutputPath, options });
